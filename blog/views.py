d3a9208715fd718630c1d24fd4bc84e64f5f75c8
Kevin Jiah-Chih Liao <me@liaokev.in>
2014-12-06 00:15:29 -0500

    Created a blog index.html that inherits the templates of the base.html inside of the pages app.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 def index(request): 
-    return HttpResponse("Hello world!")
+    return render_to_response('blog/index.html')
 
