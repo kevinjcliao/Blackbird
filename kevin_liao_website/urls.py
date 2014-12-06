3c6cf03a5adc679624225a6aa9da6db299ac854a
Kevin Jiah-Chih Liao <me@liaokev.in>
2014-12-05 23:25:02 -0500

    Solved navigation bar bug and created a base template that can be used across the entire site.

--- kevin_liao_website/urls.py
+++ kevin_liao_website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')), 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', views.about, name='about'), 
+    url(r'^home/', views.index, name='index'), 
     url(r'^$', views.index, name='index'), 
 
 )
