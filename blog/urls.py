45015f8ac2c45f17c6eb7e9597bc6e53bc45c728
Kevin Jiah-Chih Liao <me@liaokev.in>
2014-12-09 15:02:53 -0500

    Added a post details view. Images are still broken.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 
 urlpatterns = patterns('', 
         url(r'^$', views.index, name='index'), 
+        # ex: /blog/5
+        url(r'^(?P<post_id>\d+)/$', views.post, name='post'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Blog_Post
+
+def post(request, post_id): 
+    blog_post = get_object_or_404(Blog_Post, pk=post_id)
+    return render(request, 'blog/post.html', {'Blog_Post': blog_post})
 
 def index(request): 
     latest_post_list = Blog_Post.objects.order_by('-pub_date')#[:5]
